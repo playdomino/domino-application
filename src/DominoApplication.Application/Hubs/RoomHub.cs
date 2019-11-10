19a6967f70c155e10edf96f00a0a0a0b8fefea7a
Ming Li <ming.li@yunify.nl>
2019-11-10 21:43:32 +0100

    add group

--- src/DominoApplication.Application/Hubs/RoomHub.cs
+++ src/DominoApplication.Application/Hubs/RoomHub.cs
@@ -9,10 +9,10 @@ namespace DominoApplication.Application.Hubs
     public class RoomHub : Hub
     {
 
-
-        public async Task JoinRoomAction(string roomId, string userId, string name, string status)
+        public async Task JoinRoomAction(string roomId, string name, string status)
         {
-            await Clients.Client(roomId).SendAsync("JoinRoom", userId, name, status);
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            await Clients.Group(roomId).SendAsync("JoinRoom", Context.ConnectionId, name, status);
         }
     }
 }
